ccaff509d13f70dc3deb88b046aafda17035cd54
Azat Valiev <azat@valievventures.com>
2025-12-21 15:40:11 -0500

    refactor: move notifications to settings page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   const session = await getServerSession();
 
   if (session) {
-    redirect("/dashboard");
+    redirect("/app/dashboard");
   }
 
   return (

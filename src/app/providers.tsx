a809e4ed269fe3bc43537e3b050ca012e867bb3c
Azat Valiev <azat@valievventures.com>
2025-12-26 19:17:58 -0500

    use reccomended instantiation pattern

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpcClient, TRPCProvider } from "@app/lib/trpc/client";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // With SSR, we usually want to set some default staleTime
-      // above 0 to avoid refetching immediately on the client
-      staleTime: 60 * 1000,
-    },
-  },
-});
-
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <TRPCProvider queryClient={queryClient} trpcClient={trpcClient}>
